719a5eca109eb1bebccebe0fe97edacb69604e3e
Zi-Jing Zhang <zhangzj_163mail@163.com>
2026-01-04 17:38:04 +0800

    Modify  simulation/task_manager/temp.bat

--- simulation/task_manager/temp.bat
+++ simulation/task_manager/temp.bat
@@ -1,5 +1,7 @@
-cd /d F:\changeworld\PyDISK\_dirty_works\SW_linac_design\GPT_rundir\for_revise_paper\withB
-gpt -v -o swlinac.gdf swlinac.in  > 1.log 2>&1
+cd /d F:\changeworld\PyDISK\_dirty_works\HighQualityEBeamGeneration2\GPT_rundir\backup\swlinac.template_20260101022909_808136192.in
+rem gpt -v -o swlinac.gdf swlinac.in  > 1.log 2>&1
+rem start excel
+copy swlinac.gdf swlinac_copy.gdf
 gdftrans -o traj.gdf swlinac.gdf time x y z G nmacro q Bz fBz
 gdfa -v -o std.gdf swlinac.gdf position stdx stdy stdt avgG nemixrms nemiyrms nemix90 nemiy90 Q stdG
 gdf2his -b -n nmacro -o hist.gdf swlinac.gdf G 0.1
